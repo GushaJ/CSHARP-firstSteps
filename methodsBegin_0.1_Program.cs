0108d124da0298121e722de7d88caf179d9ea32f
GushaJ <gusha@grsu.by>
2019-02-17 02:15:39 +0300

    Update methodsBegin_0.1_Program.cs

--- methodsBegin_0.1_Program.cs
+++ methodsBegin_0.1_Program.cs
@@ -2,37 +2,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace methodsBegin_0.1
 {
     class Program
     {
-        private static void writeMenu(bool clearScreen)
+        //(!!!)пример описания метода для вывода в подсказках
+        /// <summary>
+        /// Вывод меню на экран с возможностью очистки экрана.
+        /// </summary>
+        /// <param name="clearScreen">флаг очистки экрана.</param>
+        /// <returns>Возвращает номер выбранного пункта меню.</returns>
+        static int MainMenu(bool clearScreen = false)
         {
             if (clearScreen)
-            {
-                Console.WriteLine("clear");
-                Thread.Sleep(1000); //приостанов, чтобы увидеть на консоли фразу "clear" 
-                Console.Clear();    //очистка экрана
-            }
-            Console.WriteLine("Main Menu:\n" +
+                Console.Clear();
+            Console.WriteLine("Main Menu.\nChoose the item - enter a number:\n" +
                             "1 - option 1;\n" +
                             "2 - option 2;\n" +
                             "3 - option 3;\n" +
                             "0 - exit.");
-        }
 
+            string userAnswer = Console.ReadLine();
+            int option;
+            if (!int.TryParse(userAnswer, out option))
+            {
+                Console.WriteLine("Wrong data!");
+                Console.ReadKey();
+                return -1;
+                //throw new NotImplementedException();
+            }
+
+            return option;
+        }
         static void Main(string[] args)
         {
             //вызов меню
-            writeMenu();
+            MainMenu(true);
 
             //работа программы
 
             //вызов меню
-            writeMenu(true);
+            MainMenu(false);
 
             Console.ReadKey();
         }
