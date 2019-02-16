9a9e0b32ebad7edc6cc053183ab9d9b18cc8b59c
GushaJ <gusha@grsu.by>
2019-02-17 02:11:03 +0300

    Update methodsBegin_0.1_Program.cs
    В пользовательском методе writeMenu() появился формальный параметр.

--- methodsBegin_0.1_Program.cs
+++ methodsBegin_0.1_Program.cs
@@ -2,47 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace methodsBegin
+namespace methodsBegin_0.1
 {
     class Program
     {
-        
-        private static void writeMenu()
+        private static void writeMenu(bool clearScreen)
         {
+            if (clearScreen)
+            {
+                Console.WriteLine("clear");
+                Thread.Sleep(1000); //приостанов, чтобы увидеть на консоли фразу "clear" 
+                Console.Clear();    //очистка экрана
+            }
             Console.WriteLine("Main Menu:\n" +
                             "1 - option 1;\n" +
                             "2 - option 2;\n" +
                             "3 - option 3;\n" +
                             "0 - exit.");
         }
-    
+
         static void Main(string[] args)
         {
             //вызов меню
             writeMenu();
-            
-
-            /*
-                Console.WriteLine("Main Menu:\n" +
-                           "1 - option 1;\n" +
-                           "2 - option 2;\n" +
-                           "3 - option 3;\n" +
-                           "0 - exit.");
-            */
 
             //работа программы
-            /*
- * здесь должен быть код программы 
- * но его нет, потому что пока не важно, что тут будет происходить
-             */
 
             //вызов меню
-            writeMenu();
-            
+            writeMenu(true);
+
             Console.ReadKey();
         }
     }
-
 }
