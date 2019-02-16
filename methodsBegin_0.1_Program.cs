7b89385df474eda44e1cecb94863566338fbaa0f
GushaJ <gusha@grsu.by>
2019-02-17 01:58:19 +0300

    Add files via upload

---
+++ methodsBegin_0.1_Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsBegin
+{
+    class Program
+    {
+        
+        private static void writeMenu()
+        {
+            Console.WriteLine("Main Menu:\n" +
+                            "1 - option 1;\n" +
+                            "2 - option 2;\n" +
+                            "3 - option 3;\n" +
+                            "0 - exit.");
+        }
+    
+        static void Main(string[] args)
+        {
+            //вызов меню
+            {    writeMenu();
+            }
+
+            {/*
+                Console.WriteLine("Main Menu:\n" +
+                           "1 - option 1;\n" +
+                           "2 - option 2;\n" +
+                           "3 - option 3;\n" +
+                           "0 - exit.");
+            */}
+            //работа программы
+
+            /*
+
+ * здесь должен быть код программы 
+ * но его нет, потому что пока не важно, что тут будет происходить
+ 
+             */
+
+            //вызов меню
+            {
+                writeMenu();
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
